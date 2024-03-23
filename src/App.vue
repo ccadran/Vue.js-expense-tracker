58348fa0372a7d92006b33905ac9cf49ae732543
ccadran <clariocadran33@gmail.com>
2024-03-23 12:39:01 +0100

    add transacxation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses" />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmited="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -15,7 +15,11 @@ import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
+import { useToast } from "vue-toastification";
+
 import { ref, computed } from "vue";
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
@@ -50,4 +54,20 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2);
 });
+
+//Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+  toast.success("Transaction added");
+  console.log(generateUniqueId());
+};
+
+//Generate a unique Id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
 </script>
